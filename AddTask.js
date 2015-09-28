ad6096d4fdbc88810a7b92790dc54186f21f4acc
backslash112 <yangcun@live.com>
2015-09-28 16:54:00 +0800

    add image picker.

--- AddTask.js
+++ AddTask.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var React = require('react-native');
+var UIImagePickerManager = require('NativeModules').UIImagePickerManager;
 
 var {
 	StyleSheet,
@@ -46,19 +47,54 @@ class AddTask extends Component {
 	render() {
 		return (
 			<View style={styles.container}>
-				<TouchableHighlight underlayColor='white'>
-					<Image source={require('image!add_image')} />
-				</TouchableHighlight>
-				<TouchableHighlight underlayColor='#dddddd'>
-					<View style={styles.saveButton}>
-						<Text>Save</Text>
-					</View>
-				</TouchableHighlight>
+			<TouchableHighlight underlayColor='white' onPress={this.onAddImagePressed.bind(this)}>
+			<Image source={require('image!add_image')} />
+			</TouchableHighlight>
+			<TouchableHighlight underlayColor='#dddddd'>
+			<View style={styles.saveButton}>
+			<Text>Save</Text>
 			</View>
-		);
+			</TouchableHighlight>
+			</View>
+			);
 	}
 
+	onAddImagePressed(event) {
+		// Specify any or all of these keys
+		var options = {
+			title: 'Select Avatar',
+			cancelButtonTitle: 'Cancel',
+			takePhotoButtonTitle: 'Take Photo...',
+			takePhotoButtonHidden: false,
+			chooseFromLibraryButtonTitle: 'Choose from Library...',
+			chooseFromLibraryButtonHidden: false,
 
+			maxWidth: 100,
+			maxHeight: 100,
+			returnBase64Image: false,
+			returnIsVertical: false,
+			quality: 0.2,
+			allowsEditing: false, 
+		};
+
+		UIImagePickerManager.showImagePicker(options, (responseType, response) => {
+			console.log(`Response Type = ${responseType}`);
+
+			if (responseType !== 'cancel') {
+				let source;
+    		if (responseType === 'data') { // New photo taken OR passed returnBase64Image true -  response is the 64 bit encoded image data string
+    			source = {uri: 'data:image/jpeg;base64,' + response, isStatic: true};
+    		}
+    		else if (responseType === 'uri') { // Selected from library - response is the URI to the local file asset
+    			source = {uri: response.replace('file://', ''), isStatic: true};
+    		}
+
+    		this.setState({
+    			avatarSource: source
+    		});
+    		}
+    	});
+	}
 }
 
 module.exports = AddTask;
---
+++ ios/UIImagePickerManager.m
@@ -0,0 +1,353 @@
+#import "UIImagePickerManager.h"
+#import "RCTConvert.h"
+
+@interface UIImagePickerManager ()
+
+@property (nonatomic, strong) UIAlertController *alertController;
+@property (nonatomic, strong) UIImagePickerController *picker;
+@property (nonatomic, strong) RCTResponseSenderBlock callback;
+@property (nonatomic, strong) NSDictionary *defaultOptions;
+@property (nonatomic, retain) NSMutableDictionary *options;
+@property (nonatomic, strong) NSDictionary *customButtons;
+
+@end
+
+@implementation UIImagePickerManager
+
+RCT_EXPORT_MODULE();
+
+- (instancetype)init
+{
+    if (self = [super init]) {
+        self.defaultOptions = @{
+            @"title": @"Select a Photo",
+            @"cancelButtonTitle": @"Cancel",
+            @"takePhotoButtonTitle": @"Take Photo...",
+            @"takePhotoButtonHidden": @NO,
+            @"chooseFromLibraryButtonTitle": @"Choose from Library...",
+            @"chooseFromLibraryButtonHidden": @NO,
+            @"returnBase64Image" : @NO, // Only return base64 encoded version of the image
+            @"returnIsVertical" : @NO, // If returning base64 image, return the orientation too
+            @"quality" : @0.2, // 1.0 best to 0.0 worst
+            @"allowsEditing" : @NO
+        };
+    }
+    return self;
+}
+
+RCT_EXPORT_METHOD(showImagePicker:(NSDictionary *)options callback:(RCTResponseSenderBlock)callback)
+{
+    self.callback = callback; // Save the callback so we can use it from the delegate methods
+    self.options = [NSMutableDictionary dictionaryWithDictionary:self.defaultOptions]; // Set default options
+    for (NSString *key in options.keyEnumerator) { // Replace default options
+        [self.options setValue:options[key] forKey:key];
+    }
+    
+    self.alertController = [UIAlertController alertControllerWithTitle:[self.options valueForKey:@"title"] message:nil preferredStyle:UIAlertControllerStyleActionSheet];
+    
+    UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:[self.options valueForKey:@"cancelButtonTitle"] style:UIAlertActionStyleCancel handler:^(UIAlertAction * action) {
+        self.callback(@[@"cancel", [NSNull null]]); // Return callback for 'cancel' action (if is required)
+    }];
+    [self.alertController addAction:cancelAction];
+
+    BOOL takePhotoHidden = [[self.options objectForKey:@"takePhotoButtonHidden"] boolValue];
+    BOOL chooseFromLibraryHidden = [[self.options objectForKey:@"chooseFromLibraryButtonHidden"] boolValue];
+    
+    if (!takePhotoHidden) {
+        UIAlertAction *takePhotoAction = [UIAlertAction actionWithTitle:[self.options valueForKey:@"takePhotoButtonTitle"] style:UIAlertActionStyleDefault handler:^(UIAlertAction * action) {
+            [self actionHandler:action];
+        }];
+        [self.alertController addAction:takePhotoAction];
+    }
+    if (!chooseFromLibraryHidden) {
+        UIAlertAction *chooseFromLibraryAction = [UIAlertAction actionWithTitle:[self.options valueForKey:@"chooseFromLibraryButtonTitle"] style:UIAlertActionStyleDefault handler:^(UIAlertAction * action) {
+            [self actionHandler:action];
+        }];
+        [self.alertController addAction:chooseFromLibraryAction];
+    }
+    
+    // Add custom buttons to action sheet
+    if([self.options objectForKey:@"customButtons"] && [[self.options objectForKey:@"customButtons"] isKindOfClass:[NSDictionary class]]){
+        self.customButtons = [self.options objectForKey:@"customButtons"];
+        for (NSString *key in self.customButtons) {
+            UIAlertAction *customAction = [UIAlertAction actionWithTitle:key style:UIAlertActionStyleDefault handler:^(UIAlertAction * action) {
+                [self actionHandler:action];
+            }];
+            [self.alertController addAction:customAction];
+        }
+    }
+    
+    dispatch_async(dispatch_get_main_queue(), ^{
+        UIViewController *root = [[[[UIApplication sharedApplication] delegate] window] rootViewController];
+        
+        /* On iPad, UIAlertController presents a popover view rather than an action sheet like on iPhone. We must provide the location
+           of the location to show the popover in this case. For simplicity, we'll just display it on the bottom center of the screen
+           to mimic an action sheet */
+        self.alertController.popoverPresentationController.sourceView = root.view;
+        self.alertController.popoverPresentationController.sourceRect = CGRectMake(root.view.bounds.size.width / 2.0, root.view.bounds.size.height, 1.0, 1.0);
+        [root presentViewController:self.alertController animated:YES completion:nil];
+    });
+
+}
+
+- (void)actionHandler:(UIAlertAction *)action
+{
+    // If button title is one of the keys in the customButtons dictionary return the value as a callback
+    if ([self.customButtons objectForKey:action.title]) {
+        self.callback(@[[self.customButtons objectForKey:action.title], [NSNull null]]);
+        return;
+    }
+    
+    self.picker = [[UIImagePickerController alloc] init];
+    if ([[self.options objectForKey:@"allowsEditing"] boolValue]) {
+        self.picker.allowsEditing = true;
+    }
+    self.picker.modalPresentationStyle = UIModalPresentationCurrentContext;
+    self.picker.delegate = self;
+    
+    if ([action.title isEqualToString:[self.options valueForKey:@"takePhotoButtonTitle"]]) { // Take photo
+        // Will crash if we try to use camera on the simulator
+#if TARGET_IPHONE_SIMULATOR
+        NSLog(@"Camera not available on simulator");
+        return;
+#else
+        self.picker.sourceType = UIImagePickerControllerSourceTypeCamera;
+#endif
+    }
+    else if ([action.title isEqualToString:[self.options valueForKey:@"chooseFromLibraryButtonTitle"]]) { // Choose from library
+        self.picker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    }
+    
+    UIViewController *root = [[[[UIApplication sharedApplication] delegate] window] rootViewController];
+    dispatch_async(dispatch_get_main_queue(), ^{
+        if (root.presentedViewController) {
+            [root.presentedViewController presentViewController:self.picker animated:YES completion:nil];
+        }
+        else {
+            [root presentViewController:self.picker animated:YES completion:nil];
+        }
+    });
+}
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
+{
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [picker dismissViewControllerAnimated:YES completion:nil];
+    });
+
+    /* Picked Image */
+    UIImage *image;
+    if ([[self.options objectForKey:@"allowsEditing"] boolValue]) {
+      image = [info objectForKey:UIImagePickerControllerEditedImage];
+    }
+    else {
+      image = [info objectForKey:UIImagePickerControllerOriginalImage];
+    }
+
+    /* creating a temp url to be passed */
+    NSString *ImageUUID = [[NSUUID UUID] UUIDString];
+    NSString *ImageName = [ImageUUID stringByAppendingString:@".jpg"];
+
+    // This will be the default URL
+    NSString* path = [[NSTemporaryDirectory()stringByStandardizingPath] stringByAppendingPathComponent:ImageName];
+    
+    NSDictionary *storageOptions;
+    // if storage options are provided change path to the documents directory
+    if([self.options objectForKey:@"storageOptions"] && [[self.options objectForKey:@"storageOptions"] isKindOfClass:[NSDictionary class]]){
+        // retrieve documents path
+        NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+        NSString *documentsDirectory = [paths objectAtIndex:0];
+        // update path to save image to documents directory
+        path = [documentsDirectory stringByAppendingPathComponent:ImageName];
+
+        storageOptions = [self.options objectForKey:@"storageOptions"];
+        // if extra path is provided try to create it
+        if ([storageOptions objectForKey:@"path"]) {
+            NSString *newPath = [documentsDirectory stringByAppendingPathComponent:[storageOptions objectForKey:@"path"]];
+            NSError *error = nil;
+            [[NSFileManager defaultManager] createDirectoryAtPath:newPath withIntermediateDirectories:YES attributes:nil error:&error];
+            
+            // if there was an error do not update path
+            if (error != nil) {
+                NSLog(@"error creating directory: %@", error);
+            }
+            else {
+                path = [newPath stringByAppendingPathComponent:ImageName];
+            }
+        }
+    }
+    
+    // Rotate the image for upload to web
+    image = [self fixOrientation:image];
+
+    //If needed, downscale image
+    float maxWidth = image.size.width;
+    float maxHeight = image.size.height;
+    if ([self.options valueForKey:@"maxWidth"]) {
+        maxWidth = [[self.options valueForKey:@"maxWidth"] floatValue];
+    }
+    if ([self.options valueForKey:@"maxHeight"]) {
+        maxHeight = [[self.options valueForKey:@"maxHeight"] floatValue];
+    }
+    image = [self downscaleImageIfNecessary:image maxWidth:maxWidth maxHeight:maxHeight];
+
+    NSData *data = UIImageJPEGRepresentation(image, [[self.options valueForKey:@"quality"] floatValue]);
+
+    BOOL BASE64 = [[self.options valueForKey:@"returnBase64Image"] boolValue];
+    BOOL returnOrientation = [self.options[@"returnIsVertical"] boolValue];
+
+    NSMutableArray *response = [[NSMutableArray alloc] init];
+
+    if (BASE64) {
+        NSString *dataString = [data base64EncodedStringWithOptions:0];
+        [response addObjectsFromArray : @[@"data", dataString]];
+    }
+    else {
+        [data writeToFile:path atomically:YES];
+        NSString *fileURL = [[NSURL fileURLWithPath:path] absoluteString];
+        // if storage options skipBackup set to true then set flag to skip icloud backup
+        if ([[storageOptions objectForKey:@"skipBackup"] boolValue]) {
+            [self addSkipBackupAttributeToItemAtPath:path];
+        }
+        [response addObjectsFromArray : @[@"uri", fileURL]];
+    }
+
+    if (returnOrientation) { // Return image orientation if desired
+        NSString *vertical = (image.size.width < image.size.height) ? @"true" : @"false";
+        [response addObject : vertical];
+    }
+
+    self.callback(response);
+}
+
+- (UIImage*)downscaleImageIfNecessary:(UIImage*)image maxWidth:(float)maxWidth maxHeight:(float)maxHeight
+{
+    UIImage* newImage = image;
+
+    //Nothing to do here?
+    if (image.size.width <= maxWidth && image.size.height <= maxHeight) {
+        return newImage;
+    }
+
+    CGSize scaledSize = CGSizeMake(image.size.width, image.size.height);
+    if (maxWidth < scaledSize.width) {
+        scaledSize = CGSizeMake(maxWidth, (maxWidth / scaledSize.width) * scaledSize.height);
+    }
+    if (maxHeight < scaledSize.height) {
+        scaledSize = CGSizeMake((maxHeight / scaledSize.height) * scaledSize.width, maxHeight);
+    }
+
+    // If the pixels are floats, it causes a white line in iOS8 and probably other versions too
+    scaledSize.width = (int)scaledSize.width;
+    scaledSize.height = (int)scaledSize.height;
+
+    UIGraphicsBeginImageContext(scaledSize); // this will resize
+    [image drawInRect:CGRectMake(0, 0, scaledSize.width, scaledSize.height)];
+    newImage = UIGraphicsGetImageFromCurrentImageContext();
+    if (newImage == nil) {
+        NSLog(@"could not scale image");
+    }
+    UIGraphicsEndImageContext();
+
+    return newImage;
+}
+
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker
+{
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [picker dismissViewControllerAnimated:YES completion:nil];
+    });
+
+    self.callback(@[@"cancel", [NSNull null]]);
+}
+
+- (UIImage *)fixOrientation:(UIImage *)srcImg {
+    if (srcImg.imageOrientation == UIImageOrientationUp) {
+        return srcImg;
+    }
+    
+    CGAffineTransform transform = CGAffineTransformIdentity;
+    switch (srcImg.imageOrientation) {
+        case UIImageOrientationDown:
+        case UIImageOrientationDownMirrored:
+            transform = CGAffineTransformTranslate(transform, srcImg.size.width, srcImg.size.height);
+            transform = CGAffineTransformRotate(transform, M_PI);
+            break;
+
+        case UIImageOrientationLeft:
+        case UIImageOrientationLeftMirrored:
+            transform = CGAffineTransformTranslate(transform, srcImg.size.width, 0);
+            transform = CGAffineTransformRotate(transform, M_PI_2);
+            break;
+
+        case UIImageOrientationRight:
+        case UIImageOrientationRightMirrored:
+            transform = CGAffineTransformTranslate(transform, 0, srcImg.size.height);
+            transform = CGAffineTransformRotate(transform, -M_PI_2);
+            break;
+        case UIImageOrientationUp:
+        case UIImageOrientationUpMirrored:
+            break;
+    }
+
+    switch (srcImg.imageOrientation) {
+        case UIImageOrientationUpMirrored:
+        case UIImageOrientationDownMirrored:
+            transform = CGAffineTransformTranslate(transform, srcImg.size.width, 0);
+            transform = CGAffineTransformScale(transform, -1, 1);
+            break;
+
+        case UIImageOrientationLeftMirrored:
+        case UIImageOrientationRightMirrored:
+            transform = CGAffineTransformTranslate(transform, srcImg.size.height, 0);
+            transform = CGAffineTransformScale(transform, -1, 1);
+            break;
+        case UIImageOrientationUp:
+        case UIImageOrientationDown:
+        case UIImageOrientationLeft:
+        case UIImageOrientationRight:
+            break;
+    }
+
+    CGContextRef ctx = CGBitmapContextCreate(NULL, srcImg.size.width, srcImg.size.height, CGImageGetBitsPerComponent(srcImg.CGImage), 0, CGImageGetColorSpace(srcImg.CGImage), CGImageGetBitmapInfo(srcImg.CGImage));
+    CGContextConcatCTM(ctx, transform);
+    switch (srcImg.imageOrientation) {
+        case UIImageOrientationLeft:
+        case UIImageOrientationLeftMirrored:
+        case UIImageOrientationRight:
+        case UIImageOrientationRightMirrored:
+            CGContextDrawImage(ctx, CGRectMake(0,0,srcImg.size.height,srcImg.size.width), srcImg.CGImage);
+            break;
+
+        default:
+            CGContextDrawImage(ctx, CGRectMake(0,0,srcImg.size.width,srcImg.size.height), srcImg.CGImage);
+            break;
+    }
+
+    CGImageRef cgimg = CGBitmapContextCreateImage(ctx);
+    UIImage *img = [UIImage imageWithCGImage:cgimg];
+    CGContextRelease(ctx);
+    CGImageRelease(cgimg);
+    return img;
+}
+
+- (BOOL)addSkipBackupAttributeToItemAtPath:(NSString *) filePathString
+{
+    NSURL* URL= [NSURL fileURLWithPath: filePathString];
+    if ([[NSFileManager defaultManager] fileExistsAtPath: [URL path]]) {
+        NSError *error = nil;
+        BOOL success = [URL setResourceValue: [NSNumber numberWithBool: YES]
+                                      forKey: NSURLIsExcludedFromBackupKey error: &error];
+        
+        if(!success){
+            NSLog(@"Error excluding %@ from backup %@", [URL lastPathComponent], error);
+        }
+        return success;
+    }
+    else {
+        NSLog(@"Error setting skip backup attribute: file not found");
+        return @NO;
+    }
+}
+
+@end
